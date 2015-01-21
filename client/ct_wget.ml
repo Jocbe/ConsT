7f7bcf5f0126c31e7c54b4d122d3360e49e3a2f6
Jocbe <jocbe@atbel.de>
2015-01-21 22:05:31 +0000

    Basic working wget using the new Abuilder library

--- client/ct_wget.ml
+++ client/ct_wget.ml
@@ -7,11 +7,26 @@ let port =
     int_of_string Sys.argv.(2)
   else
     443;;
+let path = Sys.argv.(3)
 let ts_host = "localhost"
 let ts_port = 8080;;
 
-Lwt_main.run begin
-  let ts_addr = Unix.ADDR_INET ((gethostbyname ts_host).h_addr_list.(0), ts_port) in
+let () = Lwt_main.run begin
+  lwt () = Tls_lwt.rng_init () in
+  
+  let authlet_r = Abuilder.Authlet.remote ~port:ts_port ts_host in
+  let authlet_l = Abuilder.Authlet.logger "test.log" in
+  let cnf = Abuilder.Conf.add (Abuilder.Conf.from_authlet authlet_r) authlet_l in
+  
+  lwt auth = Abuilder.Conf.build cnf in
+  lwt (ic, oc) = Tls_lwt.connect auth (host, port) in
+  
+  let req = String.concat "\r\n" [
+    "GET " ^ path ^ " HTTP/1.1" ; "Host: " ^ host  ; "Connection: close" ; "" ; ""
+  ] in 
+  Lwt_io.(write oc req >> read ic >>= printf "Got this:\n%s\n")
+
+ (* let ts_addr = Unix.ADDR_INET ((gethostbyname ts_host).h_addr_list.(0), ts_port) in
   lwt () = Tls_lwt.rng_init ()in
   let authenticator = X509.Authenticator.remote ts_addr (host, port) in
 (*  and ts_authenticator = X509_lwt.authenticator (`Ca_file "/home/jocbe/sdev/ConsT/certs/demoCA.crt") in*)
@@ -21,5 +36,5 @@ Lwt_main.run begin
   (*let ts_req = "Trying to get trust info for host " ^ host ^ " on port " ^ (string_of_int port) ^ "." in*)
   (*let ts_req = ((host, port), None) in
   Lwt_io.(write_value ts_oc ts_req >> (*print "Received:\n" >>*) read ts_ic >>= print >> print "\nDone.\n")*)
-  
+ *)
 end
---
+++ server/_build/consT_server (johann@atbel.de 2015-01-21-20-52-21).ml
@@ -0,0 +1,47 @@
+open Lwt
+
+let host = "DEFAULT HOST"
+let port = -1
+let server_cert_file = "/home/jocbe/sdev/ConsT/certs/demo-localhost.crt"
+let server_pk_file = "/home/jocbe/sdev/ConsT/certs/demo-localhost.key"
+let max_con = 16
+let myport = int_of_string Sys.argv.(1);;
+
+let worker socket sockaddr () =
+  let ic = Lwt_io.of_fd ~mode:Lwt_io.input socket in
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.output socket in
+  lwt ((host, port), cert) = Lwt_io.read_value ic in
+  let info = ref None in
+  let authenticator = X509.Authenticator.dummy info in
+  lwt (ic, oc) = Tsl_lwt.connect authenticator (host, port) in
+  Lwt_io.(close ic >> close oc);
+  let (m_host, (m_cert, m_stack)) = match info with
+    | Some (h, (c, s)) -> (h, (c, s))
+    (*| None ->*)
+  
+  Lwt_io.(printf "Host %s, port %i, m_host %s\n" host port m_host >> write_value oc (Some "Gotcha!")) >> Lwt_unix.close socket;;
+
+let rec listener (server_certs, pk) server_socket =
+  (*lwt ((ic, oc), sockaddr) = Tls_lwt.accept (server_certs, pk) server_socket in*)
+  lwt (sock, sockaddr) = Lwt_unix.accept server_socket in
+  (*let ic = Lwt_io.of_fd ~mode:Lwt_io.input sock in
+  let oc = Lwt_io.of_fd ~mode:Lwt_io.output sock in
+  Lwt.async (worker ic oc sockaddr);*)
+  Lwt.async (worker sock sockaddr);
+  listener (server_certs, pk) server_socket;;
+
+let () = Lwt_main.run begin
+  lwt () = Tls_lwt.rng_init () in
+  lwt (server_certs, pk) = X509_lwt.private_of_pems ~cert:server_cert_file ~priv_key:server_pk_file in
+  (*let (server_certs, pk) = Lwt_main.run(X509_lwt.private_of_pems ~cert:server_cert_file ~priv_key:server_pk_file) in*)
+  let server_socket = Lwt_unix.socket Lwt_unix.PF_INET Lwt_unix.SOCK_STREAM 0 in
+  let myaddr = Lwt_unix.ADDR_INET (Unix.inet_addr_of_string "127.0.0.1", myport) in
+  Lwt_unix.bind server_socket myaddr;
+  Lwt_unix.listen server_socket max_con;
+  listener (server_certs, pk) server_socket
+  
+  (*let ((ic, oc), sockaddr) = Lwt_main.run(Tls_lwt.accept (server_certs, pk) server_socket) in*)
+  (*let received = Bytes.create 4096 in
+  lwt num_rec = Lwt_io.read_into ic received 0 (Bytes.length received) in
+  Lwt_io.(printf "Received %i bytes:\n%s\n" num_rec received >> Lwt_io.close ic >> Lwt_io.close oc)*)
+end
